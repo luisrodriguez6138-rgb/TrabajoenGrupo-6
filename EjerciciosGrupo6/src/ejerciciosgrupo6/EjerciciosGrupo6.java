e0f15e90d56ec46cd55ed0ea044511d25e12d1fd
andersonsantafe6698-ops <anderson.santafe6698@utc.edu.ec>
2026-06-06 18:36:39 -0500

    Se inicializa variable factorial
    La variable almacenara el resultado final

--- EjerciciosGrupo6/src/ejerciciosgrupo6/EjerciciosGrupo6.java
+++ EjerciciosGrupo6/src/ejerciciosgrupo6/EjerciciosGrupo6.java
@@ -267,6 +267,7 @@ public class EjerciciosGrupo6 {
         System.out.println("FACTORIAL DE UN NUMERO");
         System.out.print("Ingrese un numero: ");
         int numero = sc.nextInt();
+        int factorial = 1;
 
 }
     
