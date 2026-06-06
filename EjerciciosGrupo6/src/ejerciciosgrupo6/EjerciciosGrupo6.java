b42d448d144c67bb3f473c4fc86d1c75088f8539
luisrodriguez6138-rgb <luis.rodriguez6138@utc.edu.ec>
2026-06-05 23:08:50 -0500

    eliminar hola mundo
    eliminacion

--- EjerciciosGrupo6/src/ejerciciosgrupo6/EjerciciosGrupo6.java
+++ EjerciciosGrupo6/src/ejerciciosgrupo6/EjerciciosGrupo6.java
@@ -15,7 +15,6 @@ public class EjerciciosGrupo6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola mundo");
     }
     
 }
